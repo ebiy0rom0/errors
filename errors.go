dacbbef6e7b7c79c04ec5d55db0429b561cada30
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2023-01-03 23:55:12 +0900

    [mod]refactor

--- errors.go
+++ errors.go
@@ -59,12 +59,8 @@ func (e *fundamental) Format(f fmt.State, c rune) {
 
 // Is the wrapper for the standard errors.Is().
 // It's returns same result.
-func Is(err, target error) bool {
-	return errors.Is(err, target)
-}
+func Is(err, target error) bool { return errors.Is(err, target) }
 
 // As the wrapper for the standard errors.As().
 // It's returns same result.
-func As(err error, target any) bool {
-	return errors.As(err, target)
-}
+func As(err error, target any) bool { return errors.As(err, target) }

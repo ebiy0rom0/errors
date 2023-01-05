2cd7a284bc038b6561fcc8f5ba9c851d63d07873
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2023-01-05 23:00:37 +0900

    [mod]fix test case

--- errors_test.go
+++ errors_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -88,15 +89,15 @@ func TestIs(t *testing.T) {
 		name string
 		args args
 	}{
-		{name: "std error", args: args{err: stdErr, target: fmt.Errorf("wrap: %w", stdErr)}},
-		{name: "predefined error", args: args{err: os.ErrNotExist, target: fmt.Errorf("wrap: %w", os.ErrNotExist)}},
-		{name: "lib error", args: args{err: libErr, target: fmt.Errorf("wrap: %w", libErr)}},
-		{name: "wrap std error", args: args{err: stdErr, target: Wrap(stdErr, "wrap std error")}},
-		{name: "wrap predefined error", args: args{err: os.ErrNotExist, target: Wrap(os.ErrNotExist, "wrap predefined error")}},
-		{name: "wrap lib error", args: args{err: libErr, target: Wrap(libErr, "wrap lib error")}},
-		{name: "wrapf std error", args: args{err: stdErr, target: Wrapf(stdErr, "%s", "wrapf std error")}},
-		{name: "wrapf predefined error", args: args{err: os.ErrNotExist, target: Wrapf(os.ErrNotExist, "%s", "wrapf predefined error")}},
-		{name: "wrapf lib error", args: args{err: libErr, target: Wrapf(libErr, "%s", "wrapf lib error")}},
+		{name: "std error", args: args{err: fmt.Errorf("wrap: %w", stdErr), target: stdErr}},
+		{name: "predefined error", args: args{err: fmt.Errorf("wrap: %w", os.ErrNotExist), target: os.ErrNotExist}},
+		{name: "lib error", args: args{err: fmt.Errorf("wrap: %w", libErr), target: libErr}},
+		{name: "wrap std error", args: args{err: Wrap(stdErr, "wrap std error"), target: stdErr}},
+		{name: "wrap predefined error", args: args{err: Wrap(os.ErrNotExist, "wrap predefined error"), target: os.ErrNotExist}},
+		{name: "wrap lib error", args: args{err: Wrap(libErr, "wrap lib error"), target: libErr}},
+		{name: "wrapf std error", args: args{err: Wrapf(stdErr, "%s", "wrapf std error"), target: stdErr}},
+		{name: "wrapf predefined error", args: args{err: Wrapf(os.ErrNotExist, "%s", "wrapf predefined error"), target: os.ErrNotExist}},
+		{name: "wrapf lib error", args: args{err: Wrapf(libErr, "%s", "wrapf lib error"), target: libErr}},
 		{name: "error is nil", args: args{err: nil, target: nil}},
 	}
 	for _, tt := range tests {
@@ -114,8 +115,14 @@ func TestIs(t *testing.T) {
 	}
 }
 
+type customError struct {
+	msg string
+}
+
+func (c customError) Error() string { return c.msg }
+
 func TestAs(t *testing.T) {
-	err := New("lib error")
+	err := customError{msg: "error"}
 	type args struct {
 		err    error
 		target any
@@ -124,16 +131,21 @@ func TestAs(t *testing.T) {
 		name string
 		args args
 	}{
-		{name: "lib error", args: args{err: err, target: new(fundamental)}},
-		{name: "stdwrap error", args: args{err: fmt.Errorf("wrap: %w", err), target: new(fundamental)}},
-		{name: "wrap lib error", args: args{err: Wrap(err, "wrap error"), target: new(fundamental)}},
-		{name: "wrapf lib error", args: args{err: Wrapf(err, "%s", "wrapf error"), target: new(fundamental)}},
+		{name: "lib error", args: args{err: err, target: new(customError)}},
+		{name: "stdwrap error", args: args{err: fmt.Errorf("wrap: %w", err), target: new(customError)}},
+		{name: "wrap lib error", args: args{err: Wrap(err, "wrap error"), target: new(customError)}},
+		{name: "wrapf lib error", args: args{err: Wrapf(err, "%s", "wrapf error"), target: new(customError)}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			want := errors.As(tt.args.err, &tt.args.target)
-			if got := As(tt.args.err, &tt.args.target); got != want {
+			want := errors.As(tt.args.err, tt.args.target)
+			if got := As(tt.args.err, tt.args.target); got != want {
 				t.Errorf("As() returns different results of errors.As(). result=%v, want %v", got, want)
+			}
+
+			f := tt.args.target.(*customError)
+			if !reflect.DeepEqual(err, *f) {
+				t.Errorf("set target error failed: %v", f)
 			}
 		})
 	}
